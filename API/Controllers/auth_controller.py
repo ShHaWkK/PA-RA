c99421b3de134b148f75c3f6b5963a9aab11f845
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-07-01 14:54:33 +0200

    Update authcontroller

--- API/Controllers/auth_controller.py
+++ API/Controllers/auth_controller.py
@@ -29,7 +29,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 router = APIRouter()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -77,7 +77,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise credentials_exception
     return user
 
-@router.post("/auth/token", response_model=Token)
+@router.post("/token", response_model=Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
@@ -92,6 +92,6 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-@router.get("/auth/users/me/", response_model=UserLogin)
+@router.get("/users/me/", response_model=UserLogin)
 def read_users_me(current_user: UserLogin = Depends(get_current_user)):
     return current_user
