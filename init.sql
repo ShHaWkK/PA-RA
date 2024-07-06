41c280ca4e2bccb0062fa8157412f02940b201b7
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-07-06 21:14:40 +0200

    Update add qr_code on column product

--- init.sql
+++ init.sql
@@ -75,6 +75,7 @@ CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     barcode VARCHAR(255) UNIQUE NOT NULL,
+    qr_code_path VARCHAR(255),
     expiration_date DATE NOT NULL,
     quantity INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
