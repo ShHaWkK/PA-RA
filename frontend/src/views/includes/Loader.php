bb705672ff47068d8583647d7854af231f7028d3
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-07-09 17:22:13 +0200

    Loading Multilingue

--- frontend/src/views/includes/Loader.php
+++ frontend/src/views/includes/Loader.php
@@ -1,10 +1,17 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/views/includes/lang.php'); ?>
+
+<!DOCTYPE html>
+<html lang="<?php echo strtolower($userLanguage); ?>">
 <head>
-    <title>Loading...</title>
+    <title><?php echo htmlspecialchars($data['loading_text']); ?></title>
     <link rel="stylesheet" href="/assets/css/loader.css">
 </head>
-<div id = "loading-body" class=" hidden">
-    <div class="loading-container">
-        <div class="loader"></div>
-        <p>Loading...</p>
+<body>
+    <div id="loading-body" class="hidden">
+        <div class="loading-container">
+            <div class="loader"></div>
+            <p><?php echo htmlspecialchars($data['loading_text']); ?></p>
+        </div>
     </div>
-</div>
+</body>
+</html>
