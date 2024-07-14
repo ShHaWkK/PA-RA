8c67a1ce12fc6ca333e6666e11911063cdf0cc79
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-07-14 15:04:13 +0200

    ADD 404 Styles

--- frontend/src/views/includes/404.php
+++ frontend/src/views/includes/404.php
@@ -1,1 +1,21 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/views/includes/lang.php');
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo strtolower($userLanguage); ?>">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - No More Waste</title>
+    <link rel="stylesheet" href="/assets/css/404.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHq6N2PRSDjH4rLWTv/f8K68jjg5mZDA5tNEgHf54RBIBp0/5WnZv7g0j7NJn/w3OqAtM2sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="error-container">
+        <h1>404</h1>
+        <p><?php echo htmlspecialchars($data['error_message']); ?></p>
+        <a href="/HomePage" class="btn"><i class="fa fa-home"></i> <?php echo htmlspecialchars($data['go_home']); ?></a>
+    </div>
+</body>
+</html>
