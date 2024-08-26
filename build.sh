7bacd9bcf7e95ffc0e4a8b08843645a0a898b082
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-08-26 12:54:12 +0200

    Test build 2 dockerfile for koyeb

--- build.sh
+++ build.sh
@@ -28,12 +28,24 @@ else
     exit 1
 fi
 
+# Construire l'image de la base de données
+if [ -f "./init.sql" ]; then
+    echo "Building database image..."
+    docker build -t db-image -f Dockerfile.multi .
+else
+    echo "SQL init file not found!"
+    exit 1
+fi
+
 # Exécuter les conteneurs
 echo "Running backend container..."
 docker run -d --name backend-container -p 80:80 backend-image
 
 echo "Running frontend container..."
 docker run -d --name frontend-container -p 3000:80 frontend-image
+
+echo "Running database container..."
+docker run -d --name db-container -e MYSQL_ROOT_PASSWORD=rootpassword -e MYSQL_DATABASE=your_database_name -e MYSQL_USER=your_user -e MYSQL_PASSWORD=your_password -p 3306:3306 db-image
 
 # Afficher les conteneurs en cours d'exécution
 docker ps
